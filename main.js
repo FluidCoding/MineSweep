90cfee5f61a1d4f48d26085cb05165553fbc1fb1
FluidCoding <sonnie130@gmail.com>
2016-05-08 07:04:06 -0400

    style

--- main.js
+++ main.js
@@ -97,11 +97,11 @@ init()
     }
     console.log(r,c)
 
-    if(uY!==r & a[uY][uX]===0) markZeros(uY,uX)  // upperleft
+  //  if(uY!==r & a[uY][uX]===0) markZeros(uY,uX)  // upperleft
     if(uY!==r & a[uY][c]===0) markZeros(uY,c) // up
     if(uX!==c & a[r][uX]===0)  markZeros(r,c-1)  //left
     if(dX!==c & a[r][dX]===0) markZeros(r,c+1) //right
-    if(dX!==c & dY!==r & a[dY][dX]===0) markZeros(dY,dX) //bottomright
+//    if(dX!==c & dY!==r & a[dY][dX]===0) markZeros(dY,dX) //bottomright
   //  if(uX!==c & dY!==r & a[dY][uX]===0) markZeros(dY,dX) //bottomleft
     if(dY!==r & a[dY][c]===0) markZeros(dY,c) //down
 /*
